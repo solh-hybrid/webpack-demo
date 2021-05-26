4d20fdecdb7810350ec36f8dd7f7288653c7d295
sonle <sonle@les-MacBook-Pro.local>
2021-05-26 14:38:10 +0700

    image mini

--- src/index.js
+++ src/index.js
@@ -5,17 +5,29 @@ import 'react'
 import 'angular'
 import 'd3'
 import 'highcharts'
+import 'highcharts'
+import AL_IMAGE from './images/al.png'
+
 export default function createApp() {
     // vendor()
     const el = document.createElement("div")
     el.setAttribute("id", "app")
     document.body.appendChild(el)
-    console.log("hello")
     new Vue({
         el: "#app",
         render: h => h("h1", "Hello world")
-})
+    })
+    createImage()
 }
 document.addEventListener('DOMContentLoaded', () => {
     createApp()
 })
+
+
+function createImage() {
+    const el = document.createElement("img")
+    el.setAttribute("src", AL_IMAGE)
+    el.setAttribute("width", '50%')
+    el.setAttribute("height", '50%')
+    document.body.appendChild(el)
+}
--- webpack.config.js
+++ webpack.config.js
@@ -3,20 +3,55 @@ var path = require("path");
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const smp = new SpeedMeasurePlugin();
-module.exports = smp.wrap ({
-    mode: 'development',
-    entry: {
-        app: ['./src/index.js']
-    },
-    output: {
-        filename: 'main.bundle.js',
-        path: path.join(__dirname, 'dist')
-    },
-    plugins: [
-        new webpack.DllReferencePlugin({
-            context: __dirname,
-            manifest: path.join(__dirname, 'dist', 'vendor-manifest.json')
-        }),
-        // new BundleAnalyzerPlugin()
-    ]
-})
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+
+module.exports = (_, argv) => {
+    return {
+        stats: {
+            errorDetails: true
+        },
+        mode: 'development',
+        entry: {
+            app: ['./src/index.js']
+        },
+        output: {
+            filename: 'main.bundle.js',
+            path: path.join(__dirname, 'dist'),
+            /*
+            - virtual when "webpack serve" because <image src> of image get from src
+            - When build "webpack " this is link real
+            */
+            publicPath: path.join(__dirname, 'dist/images/'),
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(jpe?g|png|gif|svg)$/i,
+                    type: 'asset'
+                },
+            ]
+        },
+        plugins: [
+            new webpack.DllReferencePlugin({
+                context: __dirname,
+                manifest: path.join(__dirname, 'dist', 'vendor-manifest.json')
+            }),
+            new ImageMinimizerPlugin({
+                severityError: 'warning', // Ignore errors on corrupted images
+                minimizerOptions: {
+                    plugins: ['gifsicle'],
+                },
+                loader: false,
+                deleteOriginalAssets: true,
+                filename: './images/[name][ext]'
+            }),
+        ],
+        devServer: {
+            contentBase: path.join(__dirname,'dist/'),
+            compress: true,
+            port: 9000,
+            allowedHosts: ['.localhost']
+        }
+    }
+}
